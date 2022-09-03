6de77e5e65e2d30996126f32a54a6ebbaeb975d1
yuki-inaho <yoshikawa@inaho.co>
2022-09-03 23:42:13 +0900

    Implement the mahalanobis distance calculation function

--- PyRobustPCA/src/pywrapper_robust_pca_impl.cpp
+++ PyRobustPCA/src/pywrapper_robust_pca_impl.cpp
@@ -9,6 +9,23 @@ using namespace nb::literals;
 using NBTensorMatrixXd = nb::tensor<nb::numpy, double, nb::shape<nb::any, nb::any>, nb::c_contig, nb::device::cpu>;
 using NBTensorVectorXd = nb::tensor<nb::numpy, double, nb::shape<nb::any>, nb::c_contig, nb::device::cpu>;
 
+double CalculateMedianNB(NBTensorVectorXd &data)
+{
+    return CalculateMedian(ConvertNBTensorToEigenVectorXd(data));
+};
+
+double CalculateMedianAbsoluteDeviationNB(NBTensorVectorXd &data)
+{
+    return CalculateMedianAbsoluteDeviation(ConvertNBTensorToEigenVectorXd(data));
+};
+
+NBTensorVectorXd CalculateMahalanobisDistanceNB(NBTensorMatrixXd &data, NBTensorVectorXd &mu, NBTensorMatrixXd &sigma_mat)
+{
+    Eigen::MatrixXd data_eigenxd = ConvertNBTensorToEigenMatrixXd(data);
+    Eigen::VectorXd mu_eigenxd = ConvertNBTensorToEigenVectorXd(mu);
+    Eigen::MatrixXd sigma_mat_eigenxd = ConvertNBTensorToEigenMatrixXd(sigma_mat);
+    return ConvertEigenVectorXdToNBTensor(CalculateMahalanobisDistance(data_eigenxd, mu_eigenxd, sigma_mat_eigenxd));
+};
 
 /* @TODO: simplify NBTensorMatrixXd?
  */
@@ -22,6 +39,7 @@ NB_MODULE(pywrapper_robust_pca_impl, m)
         .def("get_principal_components", &VanillaPCA::GetPrincipalComponents)
         .def_readonly("num_data", &VanillaPCA::num_data)
         .def_readonly("num_features", &VanillaPCA::num_features);
-    m.def("calculate_median", &CalculateMedianNB, "data"_a);
-    m.def("calculate_mad", &CalculateMedianAbsoluteDeviationNB, "data"_a);
+    m.def("median", &CalculateMedianNB, "data"_a);
+    m.def("mad", &CalculateMedianAbsoluteDeviationNB, "data"_a);
+    m.def("mahalanobis_distance", &CalculateMahalanobisDistanceNB, "data"_a, "mu"_a, "sigma_mat"_a);
 }
